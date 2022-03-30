bd2973cae9cfe71b464488809809367deca1379d
Abdur Rehman <abdur.rehman@gb3crem2722cg6.cust.communityfibre.co.uk>
2022-03-30 10:52:13 +0100

    Add cloud function

--- terraform/function.tf
+++ terraform/function.tf
@@ -20,3 +20,24 @@ resource "google_storage_bucket_object" "zip" {
     data.archive_file.source
   ]
 }
+
+# Create the Cloud function triggered by a Pub/Sub
+resource "google_cloudfunctions_function" "function" {
+  name    = var.cloud_function
+  runtime = "python39"
+
+  # Get the source code of the cloud function as a Zip compression
+  source_archive_bucket = google_storage_bucket.function_bucket.name
+  source_archive_object = google_storage_bucket_object.zip.name
+  entry_point           = "hello_pubsub"
+
+  event_trigger {
+    event_type = "google.pubsub.topic.publish"
+    resource   = "projects/${var.project_id}/topics/${var.cloud_function}"
+  }
+
+  depends_on = [
+    google_storage_bucket.function_bucket, # declared in `storage.tf`
+    google_storage_bucket_object.zip
+  ]
+}
