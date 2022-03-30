b20ae4029f619b314ea6eaceff3de365120637ca
Abdur Rehman <abdur.rehman@gb3crem2722cg6.cust.communityfibre.co.uk>
2022-03-30 10:47:56 +0100

    Add variables for terraform

---
+++ terraform/variables.tf
@@ -0,0 +1,15 @@
+variable "project_id" {
+  default = "grad-program-shared-f4fg"
+}
+
+variable "region" {
+  default = "europe-west2"
+}
+
+variable "cloud_function" {
+  default = "auto-tag-resource"
+}
+
+variable "pubsub_topic" {
+  default = "auto-tag-resource"
+}
