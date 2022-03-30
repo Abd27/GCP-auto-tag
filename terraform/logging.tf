a9192c3d3113b9fd961cb49863209b23d7d06ee8
Abdur Rehman <abdur.rehman@gb3crem2722cg6.cust.communityfibre.co.uk>
2022-03-30 11:41:23 +0100

    Add sinks for other resources

--- terraform/logging.tf
+++ terraform/logging.tf
@@ -7,3 +7,23 @@ resource "google_logging_project_sink" "auto-tag-instance" {
   # Use a unique writer (creates a unique service account used for writing)
   unique_writer_identity = true
 }
+
+resource "google_logging_project_sink" "auto-tag-storage" {
+  name        = "${var.cloud_function}-storage"
+  description = "Send logs to trigger a pub sub msg in order to run the cloud function which adds a tag to resource upon creation"
+  destination = "pubsub.googleapis.com/projects/${var.project_id}/topics/${var.pubsub_topic}"
+  filter      = "resource.type=gcs_bucket AND protoPayload.methodName=\"storage.buckets.create\""
+
+  # Use a unique writer (creates a unique service account used for writing)
+  unique_writer_identity = true
+}
+
+resource "google_logging_project_sink" "auto-tag-cluster" {
+  name        = "${var.cloud_function}-cluster"
+  description = "Send logs to trigger a pub sub msg in order to run the cloud function which adds a tag to resource upon creation"
+  destination = "pubsub.googleapis.com/projects/${var.project_id}/topics/${var.pubsub_topic}"
+  filter      = "resource.type=gke_cluster AND protoPayload.methodName=\"google.container.v1beta1.ClusterManager.CreateCluster\""
+
+  # Use a unique writer (creates a unique service account used for writing)
+  unique_writer_identity = true
+}
